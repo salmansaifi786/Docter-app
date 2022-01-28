240d4669e7ee5682ae9c598fea0fce5b8c5ead48
salmansaifi786 <salmankne@gmail.com>
2022-01-28 14:57:59 +0530

    "router added"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
-    <div className="docter">
-      <Navbar title="TextCount"/>
-      <div className="container mb-5">
-      <TextForm heading="Enter the text to analyse."/>
-      </div>
-    </div>
+    <>
+      <Router>
+        <Navbar title="TextCount" mode={mode} toggleMode={toggleMode} />
+        <div className="container mb-5">
+          <Switch>
+            <Route path="/About">
+              <About />
+            </Route>
+            <Route path="/">
+              <TextForm heading="Enter the text to analyse." mode={mode} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
 
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 export default function TextForm(props) {
-    const [text,setText] = useState('Enter text here');
+    const [text,setText] = useState('');
     const handleUpClick=()=>{
         let newText=text.toUpperCase();
         setText(newText);
@@ -15,22 +15,29 @@ export default function TextForm(props) {
         setText(event.target.value);
             
     }
+    const handleClear=()=>{
+        let newText="";
+        setText(newText);
+            
+    }
     return (
         <>
-        <div className='container'>
+        <div className="container" style={{color:props.mode==='dark'?'white':'black'}}>
+        <div className={`container text-${props.mode==='light'?'dark':'light'}`}>
             <div className="mb-4">
                 <h2>{props.heading}</h2>
-                <textarea className="form-control" id="textBox" value={text} onChange={handleChange} rows="8"></textarea>
+                <textarea className={`form-control container text-${props.mode==='light'?'dark':'light'}`} id="textBox" value={text} onChange={handleChange} rows="8" style={{backgroundColor:props.mode==='dark'?'grey':'white'}}></textarea>
             </div>
             <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Upper Case</button>
-            <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Upper Case</button>
+            <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lower Case</button>
+            <button className="btn btn-primary mx-2" onClick={handleClear}>Clear Text</button>
         </div>
         <div className="container mb-3">
             <h2>Your Text Summary</h2>
             <p>{text.split(" ").length} words and {text.length} letters</p>
             <h3>Preview</h3>
-            <p>{text}</p>
-        </div>
+            <p>{text.length>0?text:"enter your text in the box to preview"}</p>
+        </div> </div>
         </>
     )
 }
